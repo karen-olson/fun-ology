c02a53693e089b025e06c5bd9f31f94cd0036414
Karen Olson <karmarie@gmail.com>
2022-03-17 15:25:40 -0500

    Add validation and password macros to user models.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,8 @@
 class Student < ApplicationRecord
+  has_secure_password
+
   belongs_to :speech_therapist
+
+  validates :full_name, :date_of_birth, :grade, :email, :password, presence: true
+  validates :username, presence: true, uniqueness: true
 end
