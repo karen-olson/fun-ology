8449a75e7972b3a6040a9f57aae493b61a33ea66
Karen Olson <karmarie@gmail.com>
2022-03-17 15:26:15 -0500

    Seed the database with sample user data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding data"
+
+ms_olson = SpeechTherapist.create(
+    full_name: "Karen Olson",
+    email: "karmarie@gmail.com",
+    username: "karen_olson",
+    password: "password",
+    password_confirmation: "password"
+)
+
+ms_omalley = SpeechTherapist.create(
+    full_name: "Sarah O'Malley",
+    email: "sarah@gmail.com",
+    username: "sarah_omalley",
+    password: "password",
+    password_confirmation: "password"
+)
+
+steve = Student.create(
+    full_name: "Steve Jobs",
+    date_of_birth: DateTime.strptime('03.17.2009', '%m.%d.%Y'),
+    grade: 8,
+    email: "steve@gmail.com",
+    username: "steve_jobs",
+    password: "password",
+    password_confirmation: "password",
+    speech_therapist_id: ms_olson.id
+)
+
+elon = Student.create(
+    full_name: "Elon Musk",
+    date_of_birth: DateTime.strptime('11.18.2016', '%m.%d.%Y'),
+    grade: 1,
+    email: "elon@gmail.com",
+    username: "elon_musk",
+    password: "password",
+    password_confirmation: "password",
+    speech_therapist_id: ms_omalley.id
+)
+
+puts "Done seeding"
