fe314dc2a1d3aeb3226be915ed12d3560251cec0
Karen Olson <karmarie@gmail.com>
2022-03-21 20:50:51 -0500

    Add auth routes to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  
+  post "/signup/speech_therapist", to: "speech_therapists#create"
+  post "/signup/student", to: "students#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :students
   resources :speech_therapists
-  resources :test_data
+  resources :users
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
