8091094e6741616083a804be606540eaead99ebd
Karen Olson <karmarie@gmail.com>
2022-03-24 16:24:53 -0500

    Add RTK Query signup and login endpoints

--- client/src/services/phonology.js
+++ client/src/services/phonology.js
@@ -4,14 +4,65 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const phonologyApi = createApi({
   reducerPath: "phonologyApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/",
+    // credentials: "include",
+  }),
+  tagTypes: ["Login"],
   endpoints: (builder) => ({
-    getTestData: builder.query({
-      query: () => "test_data",
+    getCurrentUser: builder.query({
+      query: () => "/me",
+      providesTags: ["Login"],
     }),
+    postLogin: builder.mutation({
+      query: (loginInfo) => ({
+        url: "/login",
+        method: "POST",
+        body: loginInfo,
+      }),
+      invalidatesTags: ["Login"],
+      // example:
+      // invalidatesTags: (result, error, arg) => [{type: 'User Info', id: arg.id}|]
+    }),
+    createNewSpeechTherapist: builder.mutation({
+      query: (newSpeechTherapist) => ({
+        url: "/signup/speech_therapist",
+        method: "POST",
+        body: newSpeechTherapist,
+      }),
+    }),
+    createNewStudent: builder.mutation({
+      query: (newStudent) => ({
+        url: "/signup/student",
+        method: "POST",
+        body: newStudent,
+      }),
+    }),
+    // getSpeechTherapists: builder.query({
+
+    // })
+    // how is data stored? do I need to set up a slice and define initial state and stuff?
+    // Need to store currentUser for the entire app
+    // For other sections of the app, I need access to:
+    //    - list of students
+    //    - individual students
+    //    - list of speech therapists
+    //    - individual speech therapists (?)
+
+    // Make endpoints for:
+    //    - creating a student (mutation)
+    //    - creating a speech therapist (mutation)
+    //    - logging in (mutation)
+    //    - logging out (mutation)
+    //    - staying logged in (query)
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetTestDataQuery } = phonologyApi;
+export const {
+  useGetCurrentUserQuery,
+  usePostLoginMutation,
+  useCreateNewSpeechTherapistMutation,
+  useCreateNewStudentMutation,
+} = phonologyApi;
