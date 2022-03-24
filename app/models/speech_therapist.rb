d5aec9c23b5efdb9f018eb30e97090c2d53fa285
Karen Olson <karmarie@gmail.com>
2022-03-24 18:02:57 -0500

    Add validations to handle sign up errors

--- app/models/speech_therapist.rb
+++ app/models/speech_therapist.rb
@@ -1,3 +1,4 @@
 class SpeechTherapist < User
+    validates :username, :password, :full_name, :email, presence: true
     validates :speech_therapist_id, absence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,3 @@
 class Student < User
-    validates :speech_therapist_id, presence: true
+    validates :username, :password, :full_name, :email, :date_of_birth, :speech_therapist_id, presence: true
 end
