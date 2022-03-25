9c22c3240c82989dee5c110d6ea2f333d94c8d6f
Karen Olson <karmarie@gmail.com>
2022-03-25 09:40:23 -0500

    Fix error: not staying logged in on a refresh

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,7 +1,7 @@
 import { useGetCurrentUserQuery } from "../services/phonology";
 import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
-import NavBar from "../NavBar";
+import NavBar from "./NavBar";
 import Home from "./Home";
 import LoginForm from "../features/logIn/LoginForm";
 import StudentSignUpForm from "../features/signUp/StudentSignUpForm";
@@ -12,68 +12,48 @@ import StudentsList from "../features/student/StudentsList";
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState(null);
-  // Using a query hook automatically fetches data and returns query values
 
   useEffect(() => {
-    fetch("/me")
-      .then((resp) => resp.json())
-      .then((currentUser) => console.log(currentUser));
+    fetch("/me").then((resp) => {
+      if (resp.ok) {
+        resp.json().then((currentUser) => setCurrentUser(currentUser));
+      } else {
+        resp.json().then((err) => console.error(err));
+      }
+    });
   }, []);
 
-  // const {
-  //   data: currentUser,
-  //   isLoading,
-  //   isSuccess,
-  //   isError,
-  //   error,
-  // } = useGetCurrentUserQuery();
-
-  // console.log({ currentUser });
-  // // debugger;
-  // console.log("hello from app");
-
-  // console.log({ error });
-  // console.log({ isLoading });
-  // console.log({ isSuccess });
-  // console.log({ isError });
-  // Individual hooks are also accessible under the generated endpoints:
-  // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
+  console.log({ currentUser });
 
   if (!currentUser) {
     return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="login"
-          element={<LoginForm setCurrentUser={setCurrentUser} />}
-        />
-        <Route path="signup" element={<SignUpPage />} />
-        <Route
-          path="signup/speech-therapist"
-          element={<SpeechTherapistSignUpForm />}
-        />
-        <Route path="signup/student" element={<StudentSignUpForm />} />
-        <Route path="signup/confirmation" element={<SignUpConfirmation />} />
-      </Routes>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="login"
+            element={<LoginForm setCurrentUser={setCurrentUser} />}
+          />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route
+            path="signup/speech-therapist"
+            element={<SpeechTherapistSignUpForm />}
+          />
+          <Route path="signup/student" element={<StudentSignUpForm />} />
+          <Route path="signup/confirmation" element={<SignUpConfirmation />} />
+        </Routes>
+      </>
     );
-  } else {
+  } else if (currentUser) {
     return (
-      <div className="App">
+      <>
         <NavBar setCurrentUser={setCurrentUser} />
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-
           <Route path="students" element={<StudentsList />} />
         </Routes>
-
-        {/* {error ? (
-        <>Oh no, there was an error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-          <h3>{data}</h3>
-      ) : null} */}
-      </div>
+      </>
     );
+  } else {
+    return <h1>Loading</h1>;
   }
 }
