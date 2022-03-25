1f36d07ccbb874b7fa2454b3e935bb232095bea1
Karen Olson <karmarie@gmail.com>
2022-03-25 07:13:35 -0500

    Make sure users' username and email are unique

--- app/models/speech_therapist.rb
+++ app/models/speech_therapist.rb
@@ -1,4 +1,5 @@
 class SpeechTherapist < User
     validates :username, :password, :full_name, :email, presence: true
+    validates :username, :email, uniqueness: true
     validates :speech_therapist_id, absence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < User
     validates :username, :password, :full_name, :email, :date_of_birth, :speech_therapist_id, presence: true
+    validates :username, :email, uniqueness: true
 end
