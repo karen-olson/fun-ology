24adbe27cf38134f9f0463d47a04d5a2ee310fc1
Karen Olson <karmarie@gmail.com>
2022-03-28 14:39:51 -0400

    Create PhonemeCard component

---
+++ client/src/features/phonemes/PhonemeCard.js
@@ -0,0 +1,38 @@
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading";
+
+const PhonemeCard = ({ phonologicalProcessName, phoneme }) => {
+  const navigate = useNavigate();
+
+  function handleClick(e) {
+    console.log(`${phoneme.name} clicked`);
+    navigate(`/${phonologicalProcessName}/phonemes/${phoneme}`);
+  }
+
+  let content;
+
+  if (!phoneme) {
+    content = <Loading />;
+  } else {
+    content = (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea onClick={handleClick}>
+          <CardContent>
+            <Typography>{phoneme.name}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
+
+  return <>{content}</>;
+};
+
+export default PhonemeCard;
