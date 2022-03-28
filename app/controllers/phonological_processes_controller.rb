dacbe3fb5874193be0db8cf1392a8f8308dff59f
Karen Olson <karmarie@gmail.com>
2022-03-28 14:36:15 -0400

    Troubleshoot before_action :authorize

--- app/controllers/phonological_processes_controller.rb
+++ app/controllers/phonological_processes_controller.rb
@@ -1,6 +1,15 @@
 class PhonologicalProcessesController < ApplicationController
-    # remove
-    # skip_before_action :authorize, only: :index
+    # Should remove this
+    # Why is it trying to run authorize twice when it hits the PhonologicalProcesses#index route?
+    # Once for GET me, once for PhonProcess#index? 
+    #       The first time, the sesssion data is available
+    #       The second time, it's not available and the authorization fails. 
+    # Took off auth for PhonProcess#index - should I put it back on? 
+    # Why is GET me running when I make a fetch request in PhonProcesses?
+    #       GET me lives in a useEffect hook inside App
+    #       It should run when any page is refreshed... when else? 
+    #       Is it doing the desired behavior right now? Or no?
+    skip_before_action :authorize, only: :index
 
     def index
         phonological_processes = PhonologicalProcess.all
