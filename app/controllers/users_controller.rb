1f0458604ff86ae0ec874963578fc720828c548f
Karen Olson <karmarie@gmail.com>
2022-03-29 17:58:51 -0500

    Add users#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: :create
+    skip_before_action :authorize, only: [:create, :index]
 
     def show
         render json: @current_user
+    end
+
+    def index 
+        users = User.all 
+        render json: users 
     end
 end
