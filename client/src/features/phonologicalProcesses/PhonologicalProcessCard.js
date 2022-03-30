70d63435a34225cb62ca7290809e8bfe4c4e764f
Karen Olson <karmarie@gmail.com>
2022-03-30 14:40:31 -0500

    Add loading case to PhonologicalProcessCard

--- client/src/features/phonologicalProcesses/PhonologicalProcessCard.js
+++ client/src/features/phonologicalProcesses/PhonologicalProcessCard.js
@@ -6,24 +6,32 @@ import {
   CardActionArea,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading";
 
 const PhonologicalProcessCard = ({ phonologicalProcess }) => {
   const navigate = useNavigate();
 
   function handleClick(e) {
-    console.log(`${phonologicalProcess.name} clicked`);
     navigate(`/${phonologicalProcess.name}/phonemes`);
   }
 
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea onClick={handleClick}>
-        <CardContent>
-          <Typography>{phonologicalProcess.name}</Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
+  let content;
+
+  if (!phonologicalProcess) {
+    content = <Loading />;
+  } else {
+    content = (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea onClick={handleClick}>
+          <CardContent>
+            <Typography>{phonologicalProcess.name}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
+
+  return <>{content}</>;
 };
 
 export default PhonologicalProcessCard;
