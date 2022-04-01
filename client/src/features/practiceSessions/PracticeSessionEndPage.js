d906f5829f39d70f5e48f26eeecfc45cacc8d35c
Karen Olson <karmarie@gmail.com>
2022-04-01 17:49:54 -0500

    Start styling PracticeSessionEndPage

--- client/src/features/practiceSessions/PracticeSessionEndPage.js
+++ client/src/features/practiceSessions/PracticeSessionEndPage.js
@@ -1,7 +1,41 @@
-import React from "react";
+import { Container, Box, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const PracticeSessionEndPage = () => {
-  return <div>PracticeSessionEndPage</div>;
+  return (
+    <Container component="main" maxWidth="s">
+      <Box
+        sx={{
+          marginTop: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <br />
+        <Typography
+          component="h1"
+          variant="h3"
+          color="primary.dark"
+          fontFamily="monospace"
+        >
+          Finished!
+        </Typography>
+        <br />
+        <Typography
+          component="h1"
+          variant="h4"
+          color="primary.dark"
+          fontFamily="monospace"
+        >
+          Nice work.
+        </Typography>
+        {/* <Button as={Link} to="/login" variant="contained" sx={{ m: 2 }}> */}
+        {/* Log In
+        </Button> */}
+      </Box>
+    </Container>
+  );
 };
 
 export default PracticeSessionEndPage;
