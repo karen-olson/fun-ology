26a19874b308287a8e1f9406757f63f1c849ced5
Karen Olson <karmarie@gmail.com>
2022-04-07 11:13:23 -0500

    Add validations & msgs to PracticeSessionMinimalPair

--- app/models/practice_session_minimal_pair.rb
+++ app/models/practice_session_minimal_pair.rb
@@ -2,7 +2,10 @@ class PracticeSessionMinimalPair < ApplicationRecord
   belongs_to :practice_session
   belongs_to :minimal_pair
 
-  validates :correct, :difficulty_level, presence: true
-  validates :difficulty_level, inclusion: { in: [1, 2, 3],
+  validates :minimal_pair_id, uniqueness: { scope: :practice_session,
+    message: "Should only record one data point per minimal pair during each session" }
+  validates :correct, inclusion: { in: [true, false], 
+    message: "%{value} must be true or false"}
+  validates :difficulty_level, presence: true, inclusion: { in: [1, 2, 3],
     message: "%{value} is not a valid difficulty level. Please specify a value of 1 for easy, 2 for medium, or 3 for hard." }
 end
