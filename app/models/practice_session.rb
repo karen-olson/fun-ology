66f1f6ee8e2ad32d4cf28143e1f2047cca23943f
Karen Olson <karmarie@gmail.com>
2022-04-07 11:12:49 -0500

    Add score & difficulty level methods to PracticeSession

--- app/models/practice_session.rb
+++ app/models/practice_session.rb
@@ -4,4 +4,28 @@ class PracticeSession < ApplicationRecord
   has_many :minimal_pairs, through: :practice_session_minimal_pairs
 
   validates :type, presence: true
+
+  # custom method to calculate:
+  # score
+  
+  def average_score
+    scores = self.practice_session_minimal_pairs.map{|practice_session_minimal_pair|   
+      practice_session_minimal_pair.correct}
+
+    number_correct = scores.count(true)
+
+    average_score = ((number_correct.to_f / scores.size.to_f) * 100).truncate
+
+    self.update(score: average_score)
+  end
+
+  # average_difficulty_level
+  def average_difficulty_level
+    difficulty_levels = self.practice_session_minimal_pairs.map{|practice_session_minimal_pair|
+      practice_session_minimal_pair.difficulty_level}
+
+    average_difficulty_level = (difficulty_levels.sum.to_f / difficulty_levels.size.to_f).round.truncate
+
+    self.update(average_difficulty_level: average_difficulty_level)
+  end
 end
