b43999359fc2dbe6d648e3fefebf2ae4462e6abe
Karen Olson <karmarie@gmail.com>
2022-04-18 13:18:03 -0500

    Add view logic to StudentDataPage

--- client/src/features/student/StudentDataPage.js
+++ client/src/features/student/StudentDataPage.js
@@ -1,7 +1,60 @@
-import React from "react";
+import { Typography, Container, Box, IconButton } from "@mui/material";
+import StudentDataTable from "./StudentDataTable";
+import { useGetStudentQuery } from "../../services/phonology";
+import { useParams, useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading";
+import Error from "../../components/Error";
+import EditIcon from "@mui/icons-material/Edit";
 
 const StudentDataPage = () => {
-  return <div>StudentDataPage</div>;
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const {
+    data: student,
+    isLoading,
+    isError,
+    error,
+  } = useGetStudentQuery(params.id);
+
+  function handleEditStudentClick(e) {
+    navigate(`/students/${student.id}/edit`);
+  }
+
+  let content;
+
+  if (isLoading) {
+    content = () => {
+      return <Loading />;
+    };
+  } else if (isError) {
+    content = <Error error={error} />;
+    // fix this error message
+  } else {
+    content = (
+      <>
+        <Typography
+          variant="h4"
+          align="center"
+          fontFamily="monospace"
+          color="primary.dark"
+          sx={{ mb: 3 }}
+        >
+          {student.full_name}
+          <IconButton onClick={handleEditStudentClick}>
+            <EditIcon />
+          </IconButton>
+        </Typography>
+        <StudentDataTable />
+      </>
+    );
+  }
+
+  return (
+    <Container>
+      <Box>{content}</Box>
+    </Container>
+  );
 };
 
 export default StudentDataPage;
