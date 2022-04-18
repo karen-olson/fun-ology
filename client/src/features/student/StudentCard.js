0dd4867aefc1754e2ef81f226047b175186ff6cd
Karen Olson <karmarie@gmail.com>
2022-04-18 12:17:10 -0500

    Add view logic for StudentCard & StudentsList

---
+++ client/src/features/student/StudentCard.js
@@ -0,0 +1,46 @@
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading";
+
+const StudentCard = ({ student }) => {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(`/students/${student.id}`);
+  }
+
+  console.log(student);
+
+  let content;
+
+  if (!student) {
+    content = <Loading />;
+  } else {
+    content = (
+      <Card sx={{ maxWidth: 345, backgroundColor: "primary.dark" }}>
+        <CardActionArea onClick={handleClick}>
+          <CardContent>
+            <Typography
+              color="white"
+              fontSize="large"
+              fontFamily="monospace"
+              align="center"
+            >
+              {student.full_name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
+
+  return <>{content}</>;
+};
+
+export default StudentCard;
---
+++ client/src/features/student/StudentsList.js
@@ -0,0 +1,57 @@
+import StudentCard from "./StudentCard";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
+import { useGetStudentsQuery } from "../../services/phonology";
+import { Container, Box, Typography, Grid } from "@mui/material";
+
+const StudentList = () => {
+  const { data: students, isLoading, isError, error } = useGetStudentsQuery();
+
+  let content;
+
+  if (isLoading) {
+    content = () => {
+      return <Loading />;
+    };
+  } else if (isError) {
+    content = <Error error={error} />;
+  } else {
+    const studentCards = students.map((student) => (
+      <Grid item xs={4}>
+        <StudentCard student={student} key={student.id} />
+      </Grid>
+    ));
+
+    content = (
+      <Grid container columns={1} rowSpacing={4}>
+        {studentCards}
+      </Grid>
+    );
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          mb={3}
+          color="primary.dark"
+          fontSize="x-large"
+          fontFamily="monospace"
+          align="center"
+        >
+          Choose a Student to View Data
+        </Typography>
+        {content}
+      </Box>
+    </Container>
+  );
+};
+
+export default StudentList;
