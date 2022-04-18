2c3d0025986712679f1d5589d21b81ebafd840e6
Karen Olson <karmarie@gmail.com>
2022-04-18 12:02:45 -0500

    Add view logic to PracticeSessionEndPage

--- client/src/features/practiceSessions/PracticeSessionEndPage.js
+++ client/src/features/practiceSessions/PracticeSessionEndPage.js
@@ -1,9 +1,100 @@
-import { Container, Box, Typography, Button } from "@mui/material";
+import { useState } from "react";
+import {
+  Container,
+  Box,
+  Typography,
+  Button,
+  Grid,
+  TextField,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import DonePageGraphic from "../../DonePageGraphic.png";
 
 const PracticeSessionEndPage = () => {
+  const [showNotes, setShowNotes] = useState(false);
+  const [motes, setNotes] = useState("");
+
+  function handleShowNotesClick() {
+    setShowNotes(!showNotes);
+  }
+
+  function handleChange(e) {
+    setNotes(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // set notes on PracticeSession
+
+    console.log(e);
+  }
+
+  // fetch practice session object to get score & difficulty level
+
+  let showNotesButton;
+
+  if (showNotes === false) {
+    showNotesButton = (
+      <Button variant="contained" sx={{ mt: 2 }} onClick={handleShowNotesClick}>
+        Add Notes
+      </Button>
+    );
+  } else {
+    showNotesButton = (
+      <Button variant="contained" sx={{ mt: 2 }} onClick={handleShowNotesClick}>
+        Hide Notes
+      </Button>
+    );
+  }
+
+  let notes;
+
+  if (showNotes === true) {
+    notes = (
+      <>
+        <Typography
+          component="h1"
+          variant="h4"
+          color="primary.dark"
+          fontFamily="monospace"
+          align="center"
+        >
+          Notes
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                id="notes"
+                name="notes"
+                label="Notes"
+                type="text"
+                height="100px"
+                value={notes}
+                onChange={handleChange}
+                fullWidth
+                multiline
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 2, mb: 2 }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </>
+    );
+  } else {
+    notes = null;
+  }
+
   return (
-    <Container component="main" maxWidth="s">
+    <Container component="main" maxWidth="xl">
       <Box
         sx={{
           marginTop: 2,
@@ -19,7 +110,33 @@ const PracticeSessionEndPage = () => {
           color="primary.dark"
           fontFamily="monospace"
         >
-          Finished!
+          All Done
+        </Typography>
+        <Box
+          sx={{
+            paddingTop: 4,
+            paddingBottom: 0,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <img
+            height="200px"
+            src={DonePageGraphic}
+            alt="Clipart of striped cat jumping and saying hooray."
+            style={{
+              borderWidth: "1px",
+              borderColor: "black",
+              borderStyle: "solid",
+              borderRadius: "10px",
+            }}
+          />
+        </Box>
+        <br />
+        <Typography component="h1" variant="h5" color="primary.dark">
+          {/* Add percentage */}
+          You got ___% correct. These questions were ______ overall.
         </Typography>
         <br />
         <Typography
@@ -28,11 +145,10 @@ const PracticeSessionEndPage = () => {
           color="primary.dark"
           fontFamily="monospace"
         >
-          Nice work.
+          Keep working hard!
         </Typography>
-        {/* <Button as={Link} to="/login" variant="contained" sx={{ m: 2 }}> */}
-        {/* Log In
-        </Button> */}
+        <Box m={2}>{showNotesButton}</Box>
+        <Box m={2}>{notes}</Box>
       </Box>
     </Container>
   );
