9ef00599a4620eb9c4e0ee6b66ca40f9772e5bf3
Karen Olson <karmarie@gmail.com>
2022-04-19 14:27:17 -0500

    Add custom actions to PracticeSessions controller

--- app/controllers/practice_sessions_controller.rb
+++ app/controllers/practice_sessions_controller.rb
@@ -7,11 +7,24 @@ class PracticeSessionsController < ApplicationController
         render json: practice_session, status: :created
     end
 
-    def current_practice_session
-        current_practice_session = PracticeSession.where("current = 'true'")
+    def calculate_score
+        practice_session = PracticeSession.find(params[:id])
+        practice_session.update_score
+        practice_session.update_average_difficulty_level
+        render json: practice_session, status: :ok
+    end
 
+    def update
+        practice_session = PracticeSession.find(params[:id])
+        practice_session.update!(practice_session_params)
+        render json: practice_session, status: :ok
+    end
+
+    def get_current_practice_session
+        current_practice_session = PracticeSession.where("current = 'true'")[0]
+    
         if current_practice_session
-            render json: current_practice_session[0], status: :ok
+            render json: current_practice_session, status: :ok
         else 
             render json: "Error: No current practice session found", status: :not_found
         end
@@ -20,6 +33,6 @@ class PracticeSessionsController < ApplicationController
     private
 
     def practice_session_params
-        params.permit(:type, :date, :notes, :student_id, :current)
+        params.permit(:id, :type, :date, :notes, :student_id, :current)
     end
 end
