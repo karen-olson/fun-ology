41ca1da61b7f7f14c59266de4dc7c6b600fbc9a5
Karen Olson <karmarie@gmail.com>
2022-04-19 19:39:19 -0500

    Add serializer method to describe difficulty level

--- app/serializers/practice_session_serializer.rb
+++ app/serializers/practice_session_serializer.rb
@@ -1,4 +1,18 @@
 class PracticeSessionSerializer < ActiveModel::Serializer
-  attributes :id, :student_id, :type, :date, :notes, :score, :average_difficulty_level, :current
+  attributes :id, :student_id, :type, :date, :notes, :score, :average_difficulty_level, :average_difficulty_level_descriptor, :current
   belongs_to :student
+
+  def average_difficulty_level_descriptor
+    case self.object.average_difficulty_level
+    when 1
+      "easy"
+    when 2
+      "medium"
+    when 3
+      "hard"
+    else 
+      ""
+    end
+  end 
+  
 end
