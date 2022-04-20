b9ee4240ca173fe37e2e5047fe64c1681ffdbb27
Karen Olson <karmarie@gmail.com>
2022-04-19 19:26:08 -0500

    Clean up formatting in controllers

--- app/controllers/speech_therapists_controller.rb
+++ app/controllers/speech_therapists_controller.rb
@@ -1,16 +1,16 @@
 class SpeechTherapistsController < ApplicationController
     skip_before_action :authorize, only: [:create, :index]
 
-    def create 
-        speech_therapist = SpeechTherapist.create!(speech_therapist_params)
-        render json: speech_therapist, status: :created
-    end
-
     def index
         speech_therapists = SpeechTherapist.all 
         render json: speech_therapists 
     end
 
+    def create 
+        speech_therapist = SpeechTherapist.create!(speech_therapist_params)
+        render json: speech_therapist, status: :created
+    end
+    
     private
 
     def speech_therapist_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
     skip_before_action :authorize, only: :create
 
-    def show
-        render json: @current_user
-    end
-
     def index 
         users = User.all 
         render json: users 
+    end
+
+    def show
+        render json: @current_user
     end
 end
