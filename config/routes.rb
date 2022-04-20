1dc89888a537c642ab11c085f85177597024a382
Karen Olson <karmarie@gmail.com>
2022-04-19 19:25:20 -0500

    Add custom route: get student practice sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,34 @@
 Rails.application.routes.draw do
-  # resources :homework_sessions
-  # resources :therapy_sessions
+
   get "/students/alphabetical", to: "students#alphabetical_index"
+  get "/students/:id/practice_sessions", to: "students#practice_sessions"
+
   get "/practice_sessions/current", to: "practice_sessions#get_current_practice_session"
   patch "/practice_sessions/:id/calculate_score", to: "practice_sessions#calculate_score"
+
   post "/signup/speech_therapist", to: "speech_therapists#create"
   post "/signup/student", to: "students#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
   resources :practice_session_minimal_pairs, only: :create
   resources :practice_sessions, only: [:create, :update]
+
   resources :avatars, only: :index
+
   resources :minimal_pairs, only: :index
+
   resources :target_phonemes, only: [:index, :show]
+
   resources :phonological_processes, only: :index
+
+  resources :users, only: [:index, :show]
+  resources :speech_therapists, only: [:index, :create]
   resources :students, only: [:index, :show, :create, :update]
-  resources :speech_therapists
-  resources :users
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 end
